cc726fbc2f6721200824b02c99b70393c4596160
Jake <jecobwesterfield@gmail.com>
2017-04-26 09:41:39 -0700

    full ajax function
    added a full ajax function, for a ‘GET’ request, with a success
    property.

--- AjaxAPIpractice/js/Main.js
+++ AjaxAPIpractice/js/Main.js
@@ -1,6 +1,12 @@
 $(function () {
-    
+    var $orders = $('#orders');
     $.ajax({
-        
+        type: 'GET',
+        url: '/api/orders',
+        success: function(orders) {
+            $.each(order, function(i, order) {
+                $orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
+            });            
+        }
     });
 });
