13bc131117331b26bef4eb79aebf2e3d51e34ffb
Jake <jecobwesterfield@gmail.com>
2017-04-26 09:58:31 -0700

    update
    update

--- AjaxAPIpractice/js/Main.js
+++ AjaxAPIpractice/js/Main.js
@@ -1,5 +1,8 @@
 $(function () {
     var $orders = $('#orders');
+    var $name = $('#name');
+    var $drink = $('#drink');
+
     $.ajax({
         type: 'GET',
         url: '/api/orders',
@@ -7,6 +10,19 @@ $(function () {
             $.each(order, function(i, order) {
                 $orders.append('<li>name: '+ order.name +', drink: '+ order.drink + '</li>');
             });            
+        },
+        error: function() {
+            alert('error loading orders');
         }
     });
+
+    $('#add-order').on('click', function() {
+
+        var order = {
+            name: $name.val();
+            drink: $drink.val();
+        }
+
+    })
+
 });
